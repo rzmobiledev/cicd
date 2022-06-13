d783deda39b72f03395d4b6ecd8d34c61ad375d3
rizal <rizal@rizals-MacBook-Pro.local>
2022-06-13 22:43:39 +0700

    fixing linting flake8 2

--- app/admin/calc.py
+++ app/admin/calc.py
@@ -4,11 +4,13 @@ Calculation for running testing
 
 
 def add(x, y):
+
     """ Add y + y and return result."""
 
     return x + y
 
 def substract(x, y):
+
     """ Substract x - y and return result."""
 
     return x - y
--- app/attachment/tests/test_attachment.py
+++ app/attachment/tests/test_attachment.py
@@ -14,6 +14,7 @@ def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
 def create_attachment(user, **params):
+
     """Create and return  a sample attachment."""
 
     data = {
